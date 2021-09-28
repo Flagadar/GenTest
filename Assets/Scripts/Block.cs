b3a2818be9e6c9f5887c170459c7d65798e6648a
Rafael MARTINEZ <martinez.rafael.pro@gmail.com>
2021-09-28 18:38:37 +0200

    Rewrote the scripts

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,12 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class Block
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+
+public class Block : MonoBehaviour
 {
     public Vector3[] vertices;
-    public int[] triangles;
-    public Block(Vector3 blockOrigin, int blockCount, int chunkIndex)
+    private int[] faceFront, faceTop, faceRight, faceLeft, faceBack, faceBottom;
+	public Vector3 blockOrigin;
+	private List<Vector3> blocksPositionList = new List<Vector3>();
+
+	enum BlockID
+	{
+		AIR,
+		GRASS
+	}
+
+	public Block Init(Vector3 _blockOrigin, List<Vector3> _blocksPositionList)
+	{
+		blockOrigin = _blockOrigin;
+		blocksPositionList = _blocksPositionList;
+		return this;
+	}
+
+    void Start()
     {
+		Mesh mesh = GetComponent<MeshFilter>().mesh;
+
         vertices = new Vector3[] {
             new Vector3 (blockOrigin.x, blockOrigin.y, blockOrigin.z),
 			new Vector3 (blockOrigin.x + 1, blockOrigin.y, blockOrigin.z),
@@ -18,21 +40,59 @@ public class Block
 			new Vector3 (blockOrigin.x, blockOrigin.y, blockOrigin.z + 1),
         };
 
-		int index = (blockCount * 8) + (Chunk.verticesPerChunk * chunkIndex);
+		faceFront = new int[] {
+			0, 2, 1,
+			0, 3, 2
+		};
 
-		triangles = new int[] {
-        	index + 0, index + 2, index + 1, //face front
-			index + 0, index + 3, index + 2,
-			index + 2, index + 3, index + 4, //face top
-			index + 2, index + 4, index + 5,
-			index + 1, index + 2, index + 5, //face right
-			index + 1, index + 5, index + 6,
-			index + 0, index + 7, index + 4, //face left
-			index + 0, index + 4, index + 3,
-			index + 5, index + 4, index + 7, //face back
-			index + 5, index + 7, index + 6,
-			index + 0, index + 6, index + 7, //face bottom
-			index + 0, index + 1, index + 6
-        };
+		faceTop = new int[] {
+			2, 3, 4,
+			2, 4, 5
+		};
+
+		faceRight = new int[] {
+			1, 2, 5,
+			1, 5, 6
+		};
+
+		faceLeft = new int[] {
+			0, 7, 4,
+			0, 4, 3
+		};
+
+		faceBack = new int[] {
+			5, 4, 7,
+			5, 7, 6
+		};
+
+		faceBottom = new int[] {
+			0, 6, 7,
+			0, 1, 6
+		};
+
+		mesh.Clear();
+		GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/Wireframe");
+		mesh.vertices = vertices;
+		mesh.triangles = MeshTriangles();
+		mesh.Optimize();
+		mesh.RecalculateNormals();
     }
+
+	int[] MeshTriangles()
+	{
+		List<int> triangleList = new List<int>();
+
+		triangleList.AddRange(faceTop);
+		triangleList.AddRange(faceFront);
+		triangleList.AddRange(faceRight);
+		triangleList.AddRange(faceLeft);
+		triangleList.AddRange(faceBack);
+		triangleList.AddRange(faceBottom);
+
+
+		int[] triangleArray = new int[triangleList.Count];
+		triangleArray = triangleList.ToArray();
+
+		return triangleArray;
+	}
 }
--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -1,65 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class Chunk
+
+public class Chunk : MonoBehaviour
 {
     //Chunk size
     private const int sizeX = 16;
-    private const int sizeY = 1;
+    private const int sizeY = 2;
     private const int sizeZ = 16;
-    public const int blocksPerChunk = sizeX * sizeY * sizeZ;
 
-    public static int verticesPerChunk = blocksPerChunk * 8;
+    private List<GameObject> blocks = new List<GameObject>();
 
-    private Vector3 origin;
-    private int chunkIndex;
-    private List<Vector3> verticesList = new List<Vector3>();
-    private List<int> trianglesList = new List<int>();
+    public List<Vector3> blocksPosition = new List<Vector3>();
 
-    public Chunk(Vector3 _origin, int _chunkIndex)
+    void Start()
     {
-        chunkIndex = _chunkIndex;
-        origin = _origin;
         Init();
     }
 
     private void Init()
     {
         //Create the blocks
-        List<Block> blocks = new List<Block>();
+        int i = 0;
         for(int y = 0; y < sizeY; y++)
         {
             for(int z = 0; z < sizeZ; z++)
             {
                 for(int x = 0; x < sizeX; x++)
                 {
-                    blocks.Add(new Block(new Vector3(
-                        origin.x + x, origin.y + y, origin.z + z), 
-                        blocks.Count, chunkIndex));
+                    blocksPosition.Add(new Vector3(x, y, z));
+
+                    blocks.Add(new GameObject("Block " + i));
+                    blocks[i].AddComponent<Block>().Init(blocksPosition[i], blocksPosition);
+                    i++;
                 }
             }
         }
-
-        /* Adding vertices and triangles in lists
-           Then convert lists into arrays in the last 2 functions */
-        foreach(Block block in blocks)
-        {
-            verticesList.AddRange(block.vertices);
-            trianglesList.AddRange(block.triangles);
-        }
-    }
-
-    public Vector3[] VerticesArray()
-    {
-        Vector3[] vertices = new Vector3[verticesList.Count];
-        vertices = verticesList.ToArray();
-        return vertices;
-    }
-    
-    public int[] TrianglesArray()
-    {
-        int[] triangles = new int[trianglesList.Count];
-        triangles = trianglesList.ToArray();
-        return triangles;
     }
 }
